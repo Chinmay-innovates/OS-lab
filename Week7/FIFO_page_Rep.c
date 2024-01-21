ad314006f7c296263dad59a6dd0b7b02b2e0ba72
Chinmay-innovates <chinmaygope69@gmail.com>
2024-01-21 14:37:01 +0530

    update file name

--- Week7/FIFO_page_Rep.c
+++ Week7/FIFO_page_Rep.c
@@ -69,7 +69,7 @@ void FIFO(int pages[], int page_count)
 
 int main()
 {
-      int pages[] = {1, 3, 2, 4, 5, 5, 6, 3, 4, 3, 5, 2, 5, 3, 4, 6, 3, 7, 8, 9, 5, 4, 6, 5, 4, 6, 1, 3};
+    int pages[] = {5, 6, 7, 1, 2, 3};
     int page_count = sizeof(pages) / sizeof(pages[0]);
    printf("\nPAGE REPLACEMENT USING FIFO\n");
    FIFO(pages, page_count);
